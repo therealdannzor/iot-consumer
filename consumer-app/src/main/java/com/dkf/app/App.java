2525b92e692dded38c202e10299e4c676c045a6c
dkf <dannzor@outlook.com>
2020-08-25 11:07:57 +0100

    add maven project

---
+++ consumer-app/src/main/java/com/dkf/app/App.java
@@ -0,0 +1,13 @@
+package com.dkf.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
